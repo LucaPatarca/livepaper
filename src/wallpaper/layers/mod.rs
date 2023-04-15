8d1d79cd96a32bf769b7b2caa195be022e507035
LucaPatarca <luca.patarca@gmail.com>
2023-04-15 19:18:42 +0200

    Add layer type so that decorations do not overlap with foreground

--- src/wallpaper/layers/mod.rs
+++ src/wallpaper/layers/mod.rs
@@ -6,7 +6,15 @@ use self::{foreground::Foreground, gradient::Gradient, stars::Stars};
 use crate::config::Config;
 use std::rc::Rc;
 
+#[derive(PartialEq)]
+pub enum LayerType {
+    FOREGROUND,
+    BACKGROUND,
+    DECORATION,
+}
+
 pub trait Layer {
+    fn get_type(&self) -> LayerType;
     fn update(&mut self, hour: u8, minute: u8) -> bool;
     fn get_pixel(&self, x: u32, y: u32) -> image::Rgba<u8>;
 }
--- src/wallpaper/mod.rs
+++ src/wallpaper/mod.rs
@@ -1,3 +1,4 @@
+use self::layers::{get_layers, LayerType};
 use crate::config::Config;
 use image::{ImageBuffer, Pixel, Rgba};
 use std::rc::Rc;
@@ -5,11 +6,9 @@ use std::rc::Rc;
 mod layers;
 use layers::Layer;
 
-use self::layers::get_layers;
-
 pub struct Wallpaper {
     config: Rc<Config>,
-    layers: Vec<Box<dyn Layer>>
+    layers: Vec<Box<dyn Layer>>,
 }
 
 impl Wallpaper {
@@ -20,7 +19,11 @@ impl Wallpaper {
         }
     }
 
-    pub fn gen_wallpaper(&mut self, hour: u8, minute: u8) -> Option<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    pub fn gen_wallpaper(
+        &mut self,
+        hour: u8,
+        minute: u8,
+    ) -> Option<ImageBuffer<Rgba<u8>, Vec<u8>>> {
         let mut updated = false;
         for layer in self.layers.iter_mut() {
             updated = layer.update(hour, minute) || updated;
@@ -28,12 +31,38 @@ impl Wallpaper {
         if !updated {
             return None;
         }
-        let mut img: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(self.config.frame_widht, self.config.frame_height);
-        for (x, y, pixel) in img.enumerate_pixels_mut() {
-            for layer in self.layers.iter(){
+        let mut buf: ImageBuffer<Rgba<u8>, Vec<u8>> =
+            ImageBuffer::new(self.config.frame_widht, self.config.frame_height);
+        let foregrounds: Vec<&Box<dyn Layer>> = self
+            .layers
+            .iter()
+            .filter(|e| e.get_type() == LayerType::FOREGROUND)
+            .collect();
+        let backgrounds: Vec<&Box<dyn Layer>> = self
+            .layers
+            .iter()
+            .filter(|e| e.get_type() == LayerType::BACKGROUND)
+            .collect();
+        let decorations: Vec<&Box<dyn Layer>> = self
+            .layers
+            .iter()
+            .filter(|e| e.get_type() == LayerType::DECORATION)
+            .collect();
+        for (x, y, pixel) in buf.enumerate_pixels_mut() {
+            for layer in foregrounds.iter() {
                 pixel.blend(&layer.get_pixel(x, y));
             }
+            for layer in decorations.iter() {
+                if pixel[3] == 0 {
+                    pixel.blend(&layer.get_pixel(x, y));
+                }
+            }
+            for layer in backgrounds.iter() {
+                let mut pix = layer.get_pixel(x, y);
+                pix.blend(&pixel);
+                *pixel = pix;
+            }
         }
-        Some(img)
+        Some(buf)
     }
 }
